a6e9aba8da2d41151cca97a58240b36b11445101
AnasGoraya <anasgoraya99@gmail.com>
2026-08-31 18:02:02 +0500

    Add .gitignore and remove vendor/logs from tracking

--- extract_log.php
+++
@@ -1,9 +0,0 @@
-<?php
-$log = 'storage/logs/laravel.log';
-if (file_exists($log)) {
-    $content = file_get_contents($log, false, null, max(0, filesize($log) - 100000));
-    file_put_contents('error_report.txt', $content);
-    echo "Log extracted to error_report.txt";
-} else {
-    echo "Log file not found";
-}
